6297e1efb315ce573e9c1388302769fbd972aeeb
joaoppmdesousa <joaopedropsousa@gmail.com>
2025-06-23 17:30:26 +0100

    Add API ClientsController VehiclesController MechanicsController

---
+++ OficinaWeb/Controllers/API/ClientsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OficinaWeb.Data;
+
+namespace OficinaWeb.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : Controller
+    {
+        private readonly IClientRepository _clientRepository;
+
+        public ClientsController(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetClients()
+        {
+            return Ok(_clientRepository.GetAll());
+        }
+    }
+}
---
+++ OficinaWeb/Controllers/API/MechanicsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OficinaWeb.Data;
+
+namespace OficinaWeb.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MechanicsController : Controller
+    {
+        private readonly IMechanicRepository _mechanicRepository;
+
+        public MechanicsController(IMechanicRepository mechanicRepository)
+        {
+            _mechanicRepository = mechanicRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetVehicles()
+        {
+            return Ok(_mechanicRepository.GetAll());
+        }
+    }
+}
